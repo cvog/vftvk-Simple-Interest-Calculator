6fa89d81d845cd21ba2966de94e009bea6a69168
cvog <chris@vogiatz.is>
2022-02-28 00:20:14 +0200

    completed the styling and javascipt for the calculator

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+var slider = document.getElementById("rate");
+var output = document.getElementById("rateText");
+
+output.innerHTML  = slider.value+"%";
+slider.oninput = function() {
+    output.innerHTML = this.value+"%";
 }
-        
+
+
+function compute() {
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let targetYear = parseInt(new Date().getFullYear()) + parseInt(years);
+
+    let resultText = document.getElementById("result");
+    resultText.innerHTML = "If you deposit <span class=\"highlight\">"
+        +principal+"</span>,<br/>";
+    resultText.innerHTML += "at an interest rate of <span class=\"highlight\">"
+        +slider.value+"%</span>.<br/>";
+    resultText.innerHTML += "You will receive an amount of <span class=\"highlight\">"
+        +calculateInterest(principal, years, rate)+"</span>,<br/>";
+    resultText.innerHTML += "in the year <span class=\"highlight\">"
+        +targetYear+"</span><br/>";
+}
+
+var calculateInterest = function (principal, years, rate) {
+    return (principal*rate*years/100);
+}
